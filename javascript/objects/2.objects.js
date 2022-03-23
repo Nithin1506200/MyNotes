75d421f0bba6bb1c9c8307f303e4b5ca0ce8fb36
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2022-03-23 18:08:20 +0530

    new edit

---
+++ javascript/objects/2.objects.js
@@ -0,0 +1,39 @@
+// best to declare an object as const
+/**
+ *
+ */
+// You can create a const object:
+const car = { type: "Fiat", model: "500", color: "white" };
+// You can change a property:
+car.color = "red";
+// You can add a property:
+car.owner = "Johnson";
+
+//****************** */
+const car = { type: "Fiat", model: "500", color: "white" };
+car = { type: "Volvo", model: "EX60", color: "red" };
+// ERROR
+/*************** */
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars = ["Toyota", "Volvo", "Audi"];
+// ERROR
+
+/********* */
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+// You can change an element:
+cars[0] = "Toyota";
+// You can add an element:
+cars.push("Audi");
+/*********** */
+/********** */
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
---
+++ javascript/objects/gettersSetters.js
@@ -0,0 +1,31 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "en",
+  get lang() {
+    return this.language;
+  },
+};
+console.log(person.lang);
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+//Setters
+
+const person36 = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "",
+  set lang(lang) {
+    this.language = lang;
+  },
+};
+
+console.log(person36);
+person36.lang = "kannada";
+console.log(person36);
