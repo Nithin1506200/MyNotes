405940a774309caabed3e6aa0670f547e410d768
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2022-08-04 13:40:35 +0530

    update

---
+++ javascript/documentation/objects.js
@@ -0,0 +1,28 @@
+/**
+ * This is description of my function
+ *
+ * This is also the desctiption
+ *
+ * Syntax:
+ *
+ *  MyFunction(parm1)
+ *  MyFunction(parm2,optional)
+ *
+ * Where:
+ * - `parm: string1`
+ *  string to be passed
+ * - `optional: boolean`
+ *  Boolean
+ *
+ *
+ * @param {string} x string to be passed
+ * @param {Boolean | string} [option] optional boolen to be passed
+ * @return {string}
+ * @private
+ */
+
+function MyFunction(x, [option]) {
+  const x = "jfdkj";
+  console.log(option);
+  return x;
+}
