4067ff29041b4ee2fa9a5713ee57d81622bfe20e
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2022-08-09 10:12:11 +0530

    notes

--- javascript/interaction/script.js
+++ javascript/interaction/script.js
@@ -5,6 +5,6 @@ window.addEventListener("mousemove", (e) => {
   const h = window.innerHeight;
   const [x, y] = [e.x / w - 0.5, e.y / h - 0.5];
   for (let i of coins) {
-    i.style.transform = `rotateX(${y * 40}deg) rotateY(${x * 60}deg)`;
+    i.style.transform = `rotateX(${y * 80}deg) rotateY(${x * 50}deg)`;
   }
 });
