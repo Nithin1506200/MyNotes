a6a9cce55365ae93b9e1cadb682296a6f94884c1
Nitin N <nitin.n@saarthi.ai>
2022-12-13 19:07:24 +0530

    StrSplit

---
+++ Rust/code/rust_lang_basics/src/lifetimes/lifetimes.md
@@ -0,0 +1,8 @@
+# lifetimes
+
+- [lifetimes](#lifetimes)
+
+```
+"" this kind of string gets converted into binary and lives forever
+
+```
--- Rust/code/rust_lang_basics/src/lifetimes/lifetimes.rs
+++ Rust/code/rust_lang_basics/src/lifetimes/lifetimes.rs
@@ -1,11 +1,12 @@
+#[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
-impl StrSplit<'_> {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -14,16 +15,17 @@ impl StrSplit<'_> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delim) = self.remainder.find(self.delimiter) {
-            let until_delimiter = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-            Some(until_delimiter)
-        } else if self.remainder.is_empty() {
-            None
-        } else {
-            let rest = self.remainder;
-            self.remainder = &[];
-            Some(rest)
+        match self.remainder.as_mut() {
+            Some(remainder) => {
+                if let Some(next_delim) = remainder.find(self.delimiter) {
+                    let until_delimiter = &remainder[..next_delim];
+                    *remainder = &remainder[(next_delim + self.delimiter.len())..];
+                    Some(until_delimiter)
+                } else {
+                    self.remainder.take()
+                }
+            }
+            None => return None,
         }
     }
 }
@@ -36,6 +38,15 @@ mod test {
     fn it_works() {
         let haystack = "a b c d e";
         let letters = StrSplit::new(haystack, " ");
-        assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+        assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+        let vecmethod: Vec<&str> = StrSplit::new(haystack, " ").collect();
+        assert_eq!(vecmethod, vec!["a", "b", "c", "d", "e"]);
+    }
+    #[test]
+    fn edge_case() {
+        let haystack = "a b c d e ";
+        // let letters = StrSplit::new(haystack, " ");
+        let vecmethod: Vec<&str> = StrSplit::new(haystack, " ").collect();
+        assert_eq!(vecmethod, vec!["a", "b", "c", "d", "e", ""]);
     }
 }
